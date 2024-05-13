7a9c193c21f0d3722bcb1cc7234f53d95a2437bc
Andy Dodd <atd7@cornell.edu>
2024-05-13 12:29:04 -0400

    testclient:  WIP on haptics processing
    HERE BE DRAGONS:  There's a bunch of plotting stuff that is if(0)ed out as I anticipate needing it rarely from now on, but I don't want to lose it
    
    FIXME:  At this point this REALLY needs to be rewritten into a Python class for proper organization...  The current set of global variables is a mess

--- chiaki_testclient.py
+++ chiaki_testclient.py
@@ -6,6 +6,14 @@ import argparse
 
 from evdev import ecodes, list_devices, AbsInfo, InputDevice
 import sys, select, tty, termios
+import numpy as np
+from numpy_ringbuffer import RingBuffer
+
+import pyformulas as pf
+import matplotlib.pyplot as plt
+from matplotlib.ticker import (AutoMinorLocator, MultipleLocator)
+from time import time
+import scipy
 
 def print_event(e):
     if e.type == ecodes.EV_SYN:
@@ -101,6 +109,9 @@ def handle_event(ss,e):
                 ss.HandleButtonEvent(JoyButtons.DPAD_UP, (e.value == -1), sendImm=False)
                 ss.HandleButtonEvent(JoyButtons.DPAD_DOWN, (e.value == 1))
 
+def haptics_callback(data):
+    # requires https://github.com/eric-wieser/numpy_ringbuffer/pull/18
+    rbuf.extend(data/32767) #normalize to the equivalent of 1V p-p
 
 def main():
     ap = argparse.ArgumentParser()
@@ -117,12 +128,15 @@ def main():
 
     ss = ChiakiStreamSession(host=args['host'], regkey=regkey.data(), rpkey=morning.data())
 
+    ss.set_haptics_callback(haptics_callback)
+
     xonedev = InputDevice('/dev/input/event20')
     indevices = [xonedev]
 
     fd_to_device = {dev.fd: dev for dev in indevices}
 
     ss.Start()
+
 
     while True:
         r, w, e = select.select(fd_to_device, [], [], 0)
@@ -138,17 +152,81 @@ def main():
         Using evdev for this results in the quit function being executed any time we press q - even elsewhere.
         So read sys.stdin instead.  In the process, we've dropped support for all other keyboard inputs since controller input is fully functional now
         '''
+        global plot_pause
         for fd in r:
             if fd == sys.stdin:
                 inchar = sys.stdin.read(1)
                 if(inchar == 'q'):
                     ss.Stop()
                     return
+                if(inchar == 'p'):
+                    plot_pause = True
+                if(inchar == 'r'):
+                    plot_pause = False
+
+        global last_power_time
+        if((time() - last_power_time) >= buffer_time/2):
+            last_power_time = time()
+            lpow, hpow = get_sigpower(np.array(rbuf))
+            print("haptics low: {}, haptics high: {}".format(lpow, hpow))
+
+        global last_plot_time
+        if(0):
+            if(((time() - last_plot_time) >= plot_time) and not plot_pause):
+                last_plot_time = time()
+                ln.set_data(fdata, np.abs(fft.rfft(np.array(rbuf))))
+                fig.canvas.draw()
+                image = np.frombuffer(fig.canvas.buffer_rgba(), dtype=np.uint8)
+                image = image.reshape(fig.canvas.get_width_height()[::-1] + (4,))
+                image = np.flip(image[:,:,0:3], axis=2) #screen wants BGR, and we want to drop alpha
+                screen.update(image)
+
+# https://stackoverflow.com/questions/42169348/calculating-bandpower-of-a-signal-in-python
+def get_sigpower(data):
+    f, Pxx = scipy.signal.periodogram(np.array(data), fs=fs, scaling='density')
+    nyq = fs/2
+    ind_lowcut = np.argmax(f > 25) - 1
+    ind_highcut = np.argmax(f > 60) - 1
+    lowpow = np.sqrt(scipy.integrate.trapz(Pxx[:ind_lowcut], f[:ind_lowcut]))
+    highpow = np.sqrt(scipy.integrate.trapz(Pxx[ind_lowcut:ind_highcut], f[ind_lowcut:ind_highcut]))
+    return lowpow, highpow
 
 try:
+    buffer_time = 0.2
+    fs = 3000
+    nyq = fs/2
+    plot_pause = False
+    buf_cap = int(buffer_time*fs)
+    rbuf = RingBuffer(capacity=buf_cap, dtype=np.float32)
+    rbuf.extend(np.zeros(buf_cap))
+
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
     tty.setcbreak(sys.stdin)
+
+    if(0):
+        fig,ax = plt.subplots()
+        fig.set_figwidth(15)
+        fig.set_figheight(6)
+        fig.set_dpi(100)
+        # https://stackoverflow.com/a/49594258/4434513
+        canvas = np.zeros((480,1080,3))
+        screen = pf.screen(canvas, 'Audio Data')
+        xdata = np.arange(buf_cap)/3000
+        fdata = np.linspace(0, 3000, 513)
+        maxfreq = 150
+        ax.set_xlim(0,maxfreq)
+        ax.set_ylim(0, 32767)
+        ax.xaxis.set_major_locator(MultipleLocator(maxfreq/10))
+        ax.yaxis.set_major_locator(MultipleLocator(10000))
+        ax.xaxis.set_minor_locator(AutoMinorLocator(5))
+        ax.yaxis.set_minor_locator(AutoMinorLocator(5))
+        ax.grid(which='major', color='#CCCCCC', linestyle='--')
+        ax.grid(which='minor', color='#CCCCCC', linestyle=':')
+        ln = ax.plot(fdata, np.abs(fft.rfft(np.array(rbuf))), color='blue')[0]
+        last_plot_time = time()
+
+    last_power_time = time()
     main()
 finally:
     termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
